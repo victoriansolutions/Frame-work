3e17cd2ab89b28b19ae871c6ada7fb9fea3ae369
victoriansolutions <sathyanomula@victoriansolutionslimited.co.uk>
2025-01-06 15:21:17 +0000

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,12 @@
+
+variable "iam_role_arn" {
+}
+
+variable "bucket_name" {
+}
+
+variable "include_global_resource_types" {
+  description = "value for the include_global_resource_types attribute of the recording_group block"
+  type        = string
+  default     = "true"
+}
